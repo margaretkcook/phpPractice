859e9b79d28f6656b2d3a2a7e3f51242edb43c8c
margaretkcook <71190828+margaretkcook@users.noreply.github.com>
2020-11-02 17:34:13 -0500

    Add files via upload

---
+++ firstPHPFile.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<h1>My Firs PHP Page</h1>
+	<?php
+	echo "This was written within php<br>";
+	//all lines must end with ;
+	?>
+</body>
+</html>
